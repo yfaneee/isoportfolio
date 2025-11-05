f5e7309241e3f63154534e5e469c852e4d91ee61
Luca <lucastefan.tomescu@gmail.com>
2025-11-05 15:13:39 +0100

    cv added

--- src/components/MenuOverlay.tsx
+++ src/components/MenuOverlay.tsx
@@ -61,6 +61,18 @@ const MenuOverlay: React.FC<MenuOverlayProps> = ({ isVisible, onNavigateToLocati
               <a href="#work2" onClick={() => onNavigateToLocation?.('projects')}>Projects</a>
             </div>
           </div>
+
+          <div className="menu-section">
+            <div className="section-title-with-dividers">
+              <div className="divider-left">
+                <img src="/images/divider-003.png" alt="" />
+              </div>
+              <h3 className="cv-title-clickable" onClick={() => window.open('/pdfs/LSTCV.pdf', '_blank')}>CV</h3>
+              <div className="divider-right">
+                <img src="/images/divider-003.png" alt="" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
